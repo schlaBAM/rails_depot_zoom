7c082a2a1e0804a3728dc0bc2f68326438004521
schlaBAM <brett.morrison@shopify.com>
2020-03-05 01:16:03 -0800

    Task G Part 2 (Atom Builder) Also added functionality to disable checkout button on order paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :orders
-  resources :line_items, :carts, :products
+  resources :line_items, :carts, :orders
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'store#index', as: 'store_index'
 
