561830841c85cfd0f66ae16fc8e587fddcf15416
schlaBAM <brett.morrison@shopify.com>
2020-03-09 18:53:03 -0700

    Task I iteration 2 - adding slow payments with ActiveJob + system tests for the entire order process
    Slight change on the order form for proper wording on edit order / pass system tests
    Also upgraded Puma gem to avoid a security vulnerability

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -2,6 +2,7 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
   setup do
     @order = orders(:one)
   end
@@ -11,33 +12,19 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  # test "creating a Order" do
-  #   visit orders_url
-  #   click_on "New Order"
-  #
-  #   fill_in "Address", with: @order.address
-  #   fill_in "Email", with: @order.email
-  #   fill_in "Name", with: @order.name
-  #   fill_in "Pay type", with: @order.pay_type
-  #   click_on "Create Order"
-  #
-  #   assert_text "Order was successfully created"
-  #   click_on "Back"
-  # end
-  #
-  # test "updating a Order" do
-  #   visit orders_url
-  #   click_on "Edit", match: :first
-  #
-  #   fill_in "Address", with: @order.address
-  #   fill_in "Email", with: @order.email
-  #   fill_in "Name", with: @order.name
-  #   fill_in "Pay type", with: @order.pay_type
-  #   click_on "Update Order"
-  #
-  #   assert_text "Order was successfully updated"
-  #   click_on "Back"
-  # end
+  test "updating a Order" do
+    visit orders_url
+    click_on "Edit", match: :first
+
+    fill_in "Address", with: @order.address
+    fill_in "Email", with: @order.email
+    fill_in "Name", with: @order.name
+
+    click_on "Update Order"
+
+    assert_text "Order was successfully updated"
+    click_on "Back"
+  end
 
   test "destroying a Order" do
     visit orders_url
@@ -103,20 +90,46 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "#order_po_number"
   end
 
-  test "no pay type" do
+  test 'full order workflow' do
+    LineItem.delete_all
+    Order.delete_all
     visit store_index_url
 
     click_on 'Add to Cart', match: :first
     click_on 'Checkout'
 
-    fill_in 'order_name', with: 'Cornelius Broheim'
-    fill_in 'order_address', with: '420 Expensive Mansion Way Townville CA 90210'
-    fill_in 'order_email', with: 'doubledukes@outlook.com'
+    fill_in 'Name', with: 'Cornelius Broheim'
+    fill_in 'Address', with: '420 Expensive Mansion Way Townville CA 90210'
+    fill_in 'Email', with: 'doubledukes@outlook.com'
 
-    assert_no_selector "#order_po_number"
+    assert_no_selector "#order_routing_number"
+    assert_no_selector "#order_account_number"
 
-    select 'Purchase Order', from: 'pay_type'
+    select 'Cheque', from: 'pay_type'
 
-    assert_selector "#order_po_number"
+    assert_selector "#order_routing_number"
+    assert_selector "#order_account_number"
+
+    fill_in 'Routing Number', with: '123456'
+    fill_in 'Account Number', with: '098765'
+
+    perform_enqueued_jobs do
+      click_on 'Create Order'
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+    assert_equal 'Cornelius Broheim', order.name
+    assert_equal '420 Expensive Mansion Way Townville CA 90210', order.address
+    assert_equal 'doubledukes@outlook.com', order.email
+    assert_equal 'Cheque', order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['doubledukes@outlook.com'], mail.to
+    assert_equal 'mr fun mail <schlabam@outlook.com>', mail[:from].value
+    assert_equal 'Big Books Order Confirmation', mail.subject
   end
 end
