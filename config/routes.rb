a20544f78289be022a338f8b4cc4df62c63700b4
schlaBAM <brett.morrison@shopify.com>
2020-03-09 22:54:19 -0700

    Task J Iteration 1 - Adding Users (user scaffold + tests)
    Also did a few things around the storefront:
    - title states the controller you're on
    - clicking logo redirects to homepage
    - css color tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  resources :line_items, :carts, :orders
+  resources :line_items, :carts, :orders, :users
+
   resources :products do
     get :who_bought, on: :member
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_235544) do
+ActiveRecord::Schema.define(version: 2020_03_10_021055) do
 
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2020_03_04_235544) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 10
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
