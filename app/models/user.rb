ec44da7381f429cb81950059babcd77a934b9d87
schlaBAM <brett.morrison@shopify.com>
2020-03-10 02:51:44 -0700

    Task J Iteration 4 - implementing admin/sessions, limiting access, fixed user system tests, sidebar, preventing no admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_secure_password
+
+  after_destroy :ensure_one_admin_remains
+
+  class NoAdminsLeft < StandardError; end
+
+  private
+
+  def ensure_one_admin_remains
+    if User.count.zero?
+      raise NoAdminsLeft, "Can't delete last user. Make another one before ragekicking this one."
+    end
+  end
 end
