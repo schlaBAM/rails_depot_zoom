02c94addc25ba483e116fa80b99189c855ecaa8b
schlaBAM <brett.morrison@shopify.com>
2020-03-12 00:15:37 -0700

    task k iteration 3 - en/fr locales for everything not covered in iteration 2 (basically the rest of the app minus admin stuff)

--- app/javascript/PayTypeSelector/CreditCardPayType.jsx
+++ app/javascript/PayTypeSelector/CreditCardPayType.jsx
@@ -5,14 +5,14 @@ class CreditCardPayType extends React.Component {
         return (
             <div>
                 <div className="field">
-                    <label htmlFor="order_credit_card_number">CC Number</label>
+                    <label htmlFor="order_credit_card_number">{I18n.t('orders.form.cc_pay_type.cc_number')}</label>
                     <input type="password"
                            name="order[credit_card_number]"
                            id="order_credit_card_number"
                            autoComplete="cc-number"/>
                 </div>
                 <div className="field">
-                    <label htmlFor="order_expiration_date">Expiry</label>
+                    <label htmlFor="order_expiration_date">{I18n.t('orders.form.cc_pay_type.expiration_date')}</label>
                     <input type="text"
                            name="order[expiration_date]"
                            id="order_expiration_date"
